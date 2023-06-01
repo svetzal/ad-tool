712ae8faeac4f4e2674880ce29b6ce27e4306e5c
Stacey Vetzal <stacey@vetzal.com>
2023-06-01 07:35:24 -0400

    adding a test for a more complex filter

--- filter_expression_spec.js
+++ filter_expression_spec.js
@@ -1,10 +1,20 @@
 const expect = require('chai').expect;
 
-const {AttributeExpression} = require('./filter_expressions');
+const {AttributeExpression, FilterExpression, AndExpression, NotExpression} = require('./filter_expressions');
 
 describe("FilterExpression", () => {
     it("Can generate a simple search for an account name", () => {
         const filter = new AttributeExpression('sAMAccountName', 'username');
         expect(filter.toString()).to.equal('(sAMAccountName=username)');
+    });
+
+    it("Can generate a more complex filter expression", () => {
+        const filter = new AndExpression(
+            new AttributeExpression('title', '*developer*'),
+            new NotExpression(
+                new AttributeExpression('company', 'Acme')
+            )
+        );
+        expect(filter.toString()).to.equal('(&(title=*developer*)(!(company=Acme)))');
     });
 });
